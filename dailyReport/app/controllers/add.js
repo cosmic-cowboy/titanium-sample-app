c3588c015be04b70a1af66191114c785a61751a8
hiroo takaku <slgerkamp@gmail.com>
2015-01-17 16:39:58 +0900

    面像を投稿する #7
    作成画面に投稿ボタンを設置
    フォトギャラリーを立ち上げる
    カメラを立ち上げる

--- dailyReport/app/controllers/add.js
+++ dailyReport/app/controllers/add.js
@@ -33,8 +33,47 @@ function saveReport () {
 	}
 }
 
-
 // 投稿をキャンセル
 function close () {
 	$.add.close();
+}
+
+// ----------------------------------------------------------------
+// 画像添付
+// ----------------------------------------------------------------
+function showCamera() {
+	Ti.Media.showCamera({
+		success: function(e) {
+
+		}
+	});
+}
+
+function addPhotoPicker() {
+	Ti.Media.openPhotoGallery({
+		success: function(e) {
+			var blobMedia = e.media;
+			checkBlobProperty(blobMedia);
+
+			// ファイルの書き込み
+			var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'image.jpeg');
+			file.write(blobMedia);
+		},
+		cancel: function() {
+		}
+	});
+}
+
+// bolbについて調査
+function checkBlobProperty(blobMedia){
+	// The name of the API
+	console.log("apiName: " + blobMedia.apiName);
+	console.log("mimeType: " + blobMedia.mimeType);
+	console.log("nativePath: " + blobMedia.nativePath);
+	console.log("file: " + blobMedia.file);
+	console.log("bubbleParent: " + blobMedia.bubbleParent);
+	console.log("height: " + blobMedia.height);
+	console.log("width: " + blobMedia.width);
+	console.log("length: " + blobMedia.length);
+	console.log("text: " + blobMedia.text);
 }
